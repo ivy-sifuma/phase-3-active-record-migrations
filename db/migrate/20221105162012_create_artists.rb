0f7cd48a6934aebc5db9cae79942efdccd647fab
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-11-06 13:08:01 +0300

    Completed

--- db/migrate/20221105162012_create_artists.rb
+++ db/migrate/20221105162012_create_artists.rb
@@ -1,10 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[6.1]
   def change
-    # t.string is a method that takes a symbol as an argument and creates a column
+    create_table :artists do |t|
   t.string(:name)
   t.string(:genre)
   t.integer(:age)
   t.string(:hometown)
     end
   end
+end
 
